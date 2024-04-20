48e14cdd3a4cc94a5a093843ab932e0e36094cf8
makemi4 <dj12.exe@gmail.com>
2024-04-20 16:49:41 +0300

    modification commit: changes in stored procedures, adding new tables, etc

--- DeliverySystem/DeliverySystem.Database/dbo/Stored Procedures/p_GetDeliveryQueueRecords.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[p_GetDeliveryQueueRecords]
-	@OnlyNotCompleted bit
-AS
-	SELECT Id, Importance from dbo.DeliveryQueue q
-	LEFT JOIN Customer c on q.CustomerId = c.Id
-	where @OnlyNotCompleted = 0 OR (@OnlyNotCompleted = 1 AND IsCompleted = 0)
-
-RETURN 0
