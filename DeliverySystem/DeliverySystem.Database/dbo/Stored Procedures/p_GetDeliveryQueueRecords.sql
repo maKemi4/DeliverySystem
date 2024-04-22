b376357973799e6a39876d0b76629cf445f99ef7
makemi4 <dj12.exe@gmail.com>
2024-04-22 16:35:44 +0300

    fixed commit

---
+++ DeliverySystem/DeliverySystem.Database/dbo/Stored Procedures/p_GetDeliveryQueueRecords.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[p_GetDeliveryQueueRecords]
+	@OnlyNotCompleted bit
+AS
+	SELECT DeliveryQueueId, ImportanceRate, SubmissionDate  from dbo.DeliveryQueue
+	where @OnlyNotCompleted = 0 OR (@OnlyNotCompleted = 1 AND IsCompleted = 0)
+
+RETURN 0
